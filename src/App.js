48fc3eeab4f3024c2af0db159e8c15b9e61579c6
Ibraheem717 <78417839+Ibraheem717@users.noreply.github.com>
2025-09-29 17:39:38 +0100

    Skills section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./components/Navbar";
 import './App.css';
 import Banner from "./components/Banner";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div>
         <NavBar />
         <Banner />
+        <Skills />
     </div>
   );
 }
